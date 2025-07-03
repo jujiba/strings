1fe88d197058e7caa9fd3c1783293294b8bfa5df
jujiba <giovanna.nascimento.silva@escola.pr.gov.br>
2025-07-03 21:27:59 +0000

    Aula-5

--- script.js
+++ script.js
@@ -11,7 +11,19 @@ function mostrarpalavrachave() {
 }
 
 function processaTexto(texto){
-      let palavra = texto.split(/\P{L}+/u))
-      
+      let palavra = texto.split(/\P{L}+/u)
+
+      let frequencias = {}
+      for(let i of palavras){
+          frequencias [i] = 0;
+          for(let j of palavras){
+              if (i == j){
+              frequencias[i]++;
+            }
+        }
+      }
+
+      console.log(frequencias);
+
       return palavras;
 }
