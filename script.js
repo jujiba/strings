755594d7bf144f6f0723fbb26f56cb40ee371914
jujiba <giovanna.nascimento.silva@escola.pr.gov.br>
2025-07-03 14:48:58 +0000

    aula-2

---
+++ script.js
@@ -0,0 +1,5 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+botaoMostraPalavras.addEventListener('click', mostrarpalavrachave);
+funcion mostrarpalavrachave(){
+    alert ('fui clicado!')
+}
